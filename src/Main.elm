4568caa2ff91d5aac8e59baceb0acda44463caaf
Ryan Riegel <rriegs@gmail.com>
2019-09-25 18:39:35 -0400

    [#1] Implement adders and adder-adders, etc.
    This constitutes a draft of the intended behavior.  By working up the
    ladder, players buy progressively higher polynomial orders of growth
    with respect to time.  Later upgrades aim to achieve exponential and,
    eventually, hyperbolic growth.

--- src/Main.elm
+++ src/Main.elm
@@ -1,34 +1,79 @@
 import Browser
-import Html exposing (Html, button, text)
+import Html exposing (Html, ul, li, button, text)
 import Html.Events exposing (onClick)
+import Time
 
 
-type alias Model = Int
+type alias Model = List Int
 
 type Msg
-  = Click
+  = Tick Time.Posix
+  | Click Int
 
 
-init : Model
-init = 0
+init : () -> ( Model, Cmd Msg )
+init _ =
+  ( [0]
+  , Cmd.none
+  )
 
 
 view : Model -> Html Msg
 view model =
-  button [ onClick Click ]
-    [ text (String.fromInt model) ]
+  ul [] (List.indexedMap viewButton model)
+
+viewButton : Int -> Int -> Html Msg
+viewButton index count =
+  li []
+    [ button [ onClick (Click index) ]
+        [ text (String.fromInt count) ]
+    ]
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
-    Click ->
-      model + 1
+    Tick _ ->
+      (updateCounts model, Cmd.none)
+
+    Click index ->
+      (incAtIndex index model, Cmd.none)
+
+updateCounts : List Int -> List Int
+updateCounts counts =
+  case counts of
+    (head :: next :: tail) ->
+      (head + next :: updateCounts (next :: tail))
+
+    _ -> counts
+
+incAtIndex : Int -> List Int -> List Int
+incAtIndex index counts =
+  case (index, counts) of
+    (0, head :: tail) ->
+      (head + 1 :: (if tail == [] then [0] else tail))
+
+    (1, head :: next :: tail) ->
+      if head >= 10 then
+        (head - 10 :: next + 1 :: (if tail == [] then [0] else tail))
+      else
+        counts
+
+    (_, head :: tail) ->
+      (head :: incAtIndex (index - 1) tail)
+
+    _ -> counts
+
+
+subscriptions : Model -> Sub Msg
+subscriptions _ =
+  Time.every 1000 Tick
 
 
 main =
-  Browser.sandbox
+  Browser.element
     { init = init
     , view = view
     , update = update
+    , subscriptions = subscriptions
     }
