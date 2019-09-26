23f6a942f538afdfc55d46006bccc66214c8db8c
Ryan Riegel <rriegs@gmail.com>
2019-09-26 14:34:51 -0400

    [#1] Separate cash and workers; display worker cost
    Players now buy workers with cash rather than with 10 of the previous
    worker rank.  This mechanic seems more intuitive and should be more
    flexible with regard to upgrades, etc., in the future.

--- src/Main.elm
+++ src/Main.elm
@@ -1,68 +1,94 @@
 import Browser
-import Html exposing (Html, ul, li, button, text)
+import Html exposing (Html, div, ul, li, button, text)
 import Html.Events exposing (onClick)
 import Time
 
 
-type alias Model = List Int
+type alias Model =
+  { cash : Int
+  , workers : List Int
+  }
 
 type Msg
   = Tick Time.Posix
-  | Click Int
+  | ClickCash
+  | ClickWorker Int
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-  ( [0]
+  ( (Model 0 [0])
   , Cmd.none
   )
 
 
 view : Model -> Html Msg
 view model =
-  ul [] (List.indexedMap viewButton model)
-
-viewButton : Int -> Int -> Html Msg
-viewButton index count =
-  li []
-    [ button [ onClick (Click index) ]
-        [ text (String.fromInt count) ]
+  div []
+    [ button [ onClick ClickCash ]
+        [ text ("💰 " ++ (String.fromInt model.cash)) ]
+    , ul [] (List.indexedMap viewWorker model.workers)
     ]
+
+viewWorker : Int -> Int -> Html Msg
+viewWorker index count =
+  li []
+    [ button [ onClick (ClickWorker index) ]
+        [ text ("⛏ " ++ (String.fromInt count))
+        , text ("💰 " ++ (String.fromInt (workerCost index)))
+        ]
+    ]
+
+workerCost : Int -> Int
+workerCost index =
+  10 * 100 ^ index
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     Tick _ ->
-      (updateCounts model, Cmd.none)
+      case updateWorkers model.workers of
+        (head :: tail) ->
+          ( Model (model.cash + head) (head :: tail)
+          , Cmd.none
+          )
 
-    Click index ->
-      (incAtIndex index model, Cmd.none)
+        [] ->
+          (Model model.cash [], Cmd.none)
 
-updateCounts : List Int -> List Int
-updateCounts counts =
-  case counts of
+    ClickCash ->
+      ({model | cash = model.cash + 1}, Cmd.none)
+
+    ClickWorker index ->
+      let
+        cost = workerCost index
+      in
+        if model.cash >= cost then
+          ( Model (model.cash - cost) (incAtIndex index model.workers)
+          , Cmd.none
+          )
+
+        else (model, Cmd.none)
+
+updateWorkers : List Int -> List Int
+updateWorkers workers =
+  case workers of
     (head :: next :: tail) ->
-      (head + next :: updateCounts (next :: tail))
+      (head + next :: updateWorkers (next :: tail))
 
-    _ -> counts
+    _ -> workers
 
 incAtIndex : Int -> List Int -> List Int
-incAtIndex index counts =
-  case (index, counts) of
+incAtIndex index workers =
+  case (index, workers) of
     (0, head :: tail) ->
       (head + 1 :: (if tail == [] then [0] else tail))
-
-    (1, head :: next :: tail) ->
-      if head >= 10 then
-        (head - 10 :: next + 1 :: (if tail == [] then [0] else tail))
-      else
-        counts
 
     (_, head :: tail) ->
       (head :: incAtIndex (index - 1) tail)
 
-    _ -> counts
+    _ -> workers
 
 
 subscriptions : Model -> Sub Msg
